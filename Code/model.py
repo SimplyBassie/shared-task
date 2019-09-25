a3d24cdfea1010171590604857db457b31aebca5
runebruijn <r.k.bruijn@student.rug.nl>
2019-09-25 13:26:04 +0200

    Fixed merge pt. 2

--- Code/model.py
+++ Code/model.py
@@ -49,21 +49,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-import csv
-import pandas as pd
-
-def read_data():
-    # id	tweet	subtask_a	subtask_b	subtask_c
-    with open('../Data/olid-training-v1.0.tsv') as tsvfile:
-      file = csv.reader(tsvfile, delimiter='\t')
-      trainingdata = pd.DataFrame(file, columns=['id', 'tweet', 'subtask_a', 'subtask_b', 'subtask_c'])
-
-    return trainingdata
-
-def main():
-    dataframe = read_data()
-
-
-if __name__ == '__main__':
-    main()
