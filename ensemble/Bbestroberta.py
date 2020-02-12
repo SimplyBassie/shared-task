1e9798f407a7c3a1dba7a584bf521b299d265e5f
kaibruijn <k.a.bruijn@student.rug.nl>
2020-02-12 10:31:33 +0100

    Update and rename Bbestrune.py to Bbestroberta.py

--- ensemble/Bbestroberta.py
+++ ensemble/Bbestroberta.py
@@ -6,7 +6,7 @@ from hashtags import *
 from emoji_to_words import *
 
 
-def Bbestrune():
+def Bbestroberta():
     architecture = 'roberta'
     model_type = 'roberta-base'
     subtask = 'B'
@@ -78,7 +78,7 @@ def Bbestrune():
     # Evaluate the model
     result, model_outputs, wrong_predictions = model.eval_model(eval_df, cr=classification_report, cm=confusion_matrix)
 
-    print("Best model Rune task B:")
+    print("Best model roberta task B:")
     print(model_outputs)
     print(result['cr']) # Classification report
     print(result['cm']) # Confusion matrix
